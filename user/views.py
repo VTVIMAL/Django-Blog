567839c93dcd46beeb29e4b0f12e8f3e802ad378
Vimal Prasad V T <vimal.vt.prasad@gmail.com>
2022-12-31 13:09:55 +0530

    Ready for Deployment

---
+++ user/views.py
@@ -0,0 +1,44 @@
+from multiprocessing import context
+from winreg import REG_WHOLE_HIVE_VOLATILE
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Successfully created account for {username}! ")
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {"form":form})
+
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"You have Updated your profile")
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    
+    context = {
+
+        "u_form":u_form,
+        'p_form':p_form
+    }
+
+    return render(request, 'user/profile.html', context)
